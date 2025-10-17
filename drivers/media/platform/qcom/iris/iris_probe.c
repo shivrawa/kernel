1a7aeec169892b24bcce1d93e4f1c72e4516c9ed
Vikash Garodia <vikash.garodia@oss.qualcomm.com>
2025-10-17 19:46:29 +0530

    FROMLIST: media: iris: Add platform data for kaanapali
    Add support for the kaanapali platform by re-using the SM8550
    definitions and using the vpu4 ops.
    Move the configurations that differs in a per-SoC platform
    header, that will contain SoC specific data.
    
    Link: https://lore.kernel.org/all/20251017-knp_video-v2-8-f568ce1a4be3@oss.qualcomm.com/
    Co-developed-by: Vishnu Reddy <quic_bvisredd@quicinc.com>
    Signed-off-by: Vishnu Reddy <quic_bvisredd@quicinc.com>
    Signed-off-by: Vikash Garodia <vikash.garodia@oss.qualcomm.com>
    Reviewed-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- drivers/media/platform/qcom/iris/iris_probe.c
+++ drivers/media/platform/qcom/iris/iris_probe.c
@@ -349,6 +349,10 @@ static const struct dev_pm_ops iris_pm_ops = {
 
 static const struct of_device_id iris_dt_match[] = {
 	{
+		.compatible = "qcom,kaanapali-iris",
+		.data = &kaanapali_data,
+	},
+	{
 		.compatible = "qcom,qcs8300-iris",
 		.data = &qcs8300_data,
 	},
