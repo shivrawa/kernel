4366088b3f14ce6389e20256da357ac64bc35df9
Gourav Kumar <gouravk@qti.qualcomm.com>
2026-05-23 09:25:44 +0530

    Revert "FROMLIST: media: qcom: iris: drop hw_response_timeout_val from platform data"
    This reverts commit a40da675a7582a5ba95455aaab56b9557312c071.
    
    Signed-off-by: Gourav Kumar <gouravk@qti.qualcomm.com>

--- drivers/media/platform/qcom/iris/iris_platform_common.h
+++ drivers/media/platform/qcom/iris/iris_platform_common.h
@@ -246,6 +246,7 @@ struct iris_platform_data {
 	const struct tz_cp_config *tz_cp_config_data;
 	u32 tz_cp_config_data_size;
 	u32 core_arch;
+	u32 hw_response_timeout;
 	struct ubwc_config_data *ubwc_config;
 	u32 num_vpp_pipe;
 	bool no_aon;
